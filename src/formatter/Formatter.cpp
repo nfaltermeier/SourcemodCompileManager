3b92a411d6d69fd7ff83a5afb5d411ee2354aeb8
Nathan <nefalt2@gmail.com>
2018-04-26 09:59:53 -0500

    Program finds compiler version and work on finding errors/warnings

--- src/formatter/Formatter.cpp
+++ src/formatter/Formatter.cpp
@@ -10,14 +10,27 @@
 
 std::string Formatter::ProcessResults(std::vector<CompileResult> results)
 {
-	std::string output;
+	std::string output = "Sourcemod Compile Manager\n";
+	std::istringstream f(results[0].output);
+	std::string line;
+	int warnings;
+	int errors;
+	while (std::getline(f, line)) {
+		if(line.find("SourcePawn Compiler ") != std::string::npos) {
+			output += "Sourcemod Version: " + line.substr(20);
+		}
+	}
 
 	for(CompileResult result : results)
 	{
 		std::istringstream f(result.output);
 		std::string line;
 		while (std::getline(f, line)) {
-			std::cout << line << std::endl;
+			if(line.find(" : warning ")) {
+
+			} else if(line.find(" : error ")) {
+
+			}
 		}
 	}
 
