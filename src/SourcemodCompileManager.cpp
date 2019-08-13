db88c7396a8e9995ef5d71e850fab24e8e87d752
Blackop778 <nefalt2@gmail.com>
2019-08-13 02:34:01 -0500

    Check to make sure compiler is a file

--- src/SourcemodCompileManager.cpp
+++ src/SourcemodCompileManager.cpp
@@ -1,10 +1,12 @@
 
-#include "base/Compiler.h"
 // https://github.com/Taywee/args
 #include <args.hxx>
 #include <iostream>
 #include <chrono>
 #include <unistd.h>
+#include <sys/stat.h>
+
+#include "base/Compiler.h"
 #include "base/SCMErrors.h"
 #include "formatter/Formatter.h"
 
@@ -40,7 +42,19 @@ int main(int argc, char **argv) {
     std::string compiler = compilerOption.Get();
 
     if (access(compiler.c_str(), F_OK) != 0) {
-        std::cerr << "Could not find compiler at '" << compiler << "'" << std::endl;
+        std::cerr << "'" << compiler << "' does not exist (could not find compiler)" << std::endl;
+        return 1;
+    }
+
+    struct stat compilerStat;
+    if (stat(compiler.c_str(), &compilerStat) != 0) {
+        std::cerr << "Something went wrong when checking access to the compiler at '" << compiler << "'" << std::endl;
+        return 1;
+    }
+
+    // path does not specify a file
+    if (!(compilerStat.st_mode & S_IFREG)) {
+        std::cerr << "'" << compiler << "' does not specify a file" << std::endl;
         return 1;
     }
 
